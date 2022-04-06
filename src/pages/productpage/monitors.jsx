8f2b36be404a6d9a36c3dba2fcf0d1156ca465b9
Aatish Pandey <32216307+aatishpandey@users.noreply.github.com>
2022-04-06 13:40:57 +0530

    feat:added styles and strucutre for product page and for diff. categories

---
+++ src/pages/productpage/monitors.jsx
@@ -0,0 +1,37 @@
+import "./product-page.css"
+import { BannerCard, Filters, useProduct, sortList, filterRating, filterSlider, Loader, filterCategory, useCart } from "./productUtil";
+
+const MonitorsPage = () => {
+    const { productList, loading } = useCart();
+    const { state } = useProduct();
+    const { rating, sortBy, gender, sliderVal, category } = state;
+    const sliderFilteredList = filterSlider(productList, sliderVal)
+    const priceSortedList = sortList(sliderFilteredList, sortBy);
+    const ratingFilteredList = filterRating(priceSortedList, rating);
+    const categoryFilteredList = filterCategory(ratingFilteredList, category);
+
+    const renderList = categoryFilteredList.filter(item => item.category === "monitor")
+    return (
+        <div class="main-product">
+            <Filters
+                keyboardDisabled={true}
+                headphoneDisabled={true}
+            />
+            <div class="product-container">
+                <div className="sidebar-head">Showing All Products <i className="sidebar-text">(Showing {renderList.length} products)</i>
+                </div>
+                <div className="product-list">
+                    {loading && <Loader />}
+                    {renderList.map((product) => (
+                        <BannerCard
+                            key={product.id}
+                            product={product} />
+                    ))}
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export { MonitorsPage }
---
+++ src/pages/productpage/product-page.jsx
@@ -0,0 +1,34 @@
+import "./product-page.css"
+import { BannerCard, Filters, useProduct, sortList, filterRating, filterSlider, Loader, filterCategory, useCart } from "./productUtil";
+
+const ProductPage = () => {
+    const { productList, loading } = useCart();
+    const { state } = useProduct();
+    const { rating, sortBy, gender, sliderVal, category } = state;
+    const sliderFilteredList = filterSlider(productList, sliderVal)
+    const priceSortedList = sortList(sliderFilteredList, sortBy);
+    const ratingFilteredList = filterRating(priceSortedList, rating);
+    const categoryFilteredList = filterCategory(ratingFilteredList, category);
+
+
+    return (
+        <div class="main-product">
+            <Filters />
+            <div class="product-container">
+                <div className="sidebar-head">Showing All Products <i className="sidebar-text">(Showing {categoryFilteredList.length} products)</i>
+                </div>
+                <div className="product-list">
+                    {loading && <Loader />}
+                    {categoryFilteredList.map((product) => (
+                        <BannerCard
+                            key={product.id}
+                            product={product} />
+                    ))}
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export { ProductPage }
