233edc2f356626b7d20b382c42b2a883bba2e2aa
Aatish Pandey <32216307+aatishpandey@users.noreply.github.com>
2022-04-06 13:38:36 +0530

    feat : added styles and structure for product card

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import "./Banner.css"
+
+const Banner = () => {
+    return (
+        <div className="banner-container">
+            <img src="assets/banner-hero.svg" alt="banner hero" className="banner-hero"/>
+        </div>
+    )
+}
+
+export { Banner }
